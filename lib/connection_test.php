e1a8c5e5ed35d39d649b5adba4df49d382004f89
xbexbex <petja.valkama@gmail.com>
2017-09-05 23:08:55 +0300

    css ja html

--- lib/connection_test.php
+++ lib/connection_test.php
@@ -10,69 +10,90 @@
 
 $connection_config = DatabaseConfig::connection_config();
 
-function getDatabase() {
-  return DB::connection();
+function getDatabase()
+{
+    return DB::connection();
 }
-function getObjects() {
-  $args = func_get_args();
-  $sql = array_shift($args);
-  $query = getDatabase()->prepare($sql);
-  $query->execute($args);
-  return $query->fetchAll(PDO::FETCH_OBJ);
+function getObjects()
+{
+    $args = func_get_args();
+    $sql = array_shift($args);
+    $query = getDatabase()->prepare($sql);
+    $query->execute($args);
+    return $query->fetchAll(PDO::FETCH_OBJ);
 }
-function getValue() {
-  $args = func_get_args();
-  $sql = array_shift($args);
-  $query = getDatabase()->prepare($sql);
-  $query->execute($args);
-  return $query->fetchColumn();
+function getValue()
+{
+    $args = func_get_args();
+    $sql = array_shift($args);
+    $query = getDatabase()->prepare($sql);
+    $query->execute($args);
+    return $query->fetchColumn();
 }
 
-class TableLister {
-  protected $name;
+class TableLister
+{
+    protected $name;
 
-  public function __construct($name) {$this->name = $name;}
-  public function getName() { return $this->name; }
-  public function quoteName() { return static::TABLE_QUOTE.$this->name.static::TABLE_QUOTE; }
-
-  public function getRowCount() {
-    return getValue('SELECT count(*) FROM '.$this->quoteName());
-  }
-  public function getRows($limit = 100) {
-    return getObjects("SELECT * FROM ".$this->quoteName()." LIMIT ".(int)$limit);
-  }
-}
-class MySqlTableLister extends TableLister {
-  const TABLE_QUOTE = '`';
-
-  public static function getTables() {
-    $objs = getObjects("SHOW TABLES");
-    $ret = array();
-    foreach($objs as $obj) {
-      foreach($obj as $v) {
-        $ret[] = new self($v);
-        break;
-      }
+    public function __construct($name)
+    {
+        $this->name = $name;
     }
-    return $ret;
-  }
-  public function getColumns() {
-    return getObjects("DESCRIBE ".$this->quoteName());
-  }
-}
-class PgSqlTableLister extends TableLister {
-  const TABLE_QUOTE = '"';
-
-  public static function getTables() {
-    $objs = getObjects("select tablename from pg_tables where tableowner != 'postgres' order by tablename");
-    $ret = array();
-    foreach($objs as $obj) {
-      $ret[] = new self($obj->tablename);
+    public function getName()
+    {
+        return $this->name;
     }
-    return $ret;
-  }
-  public function getColumns() {
-    $sql = <<<SQL
+    public function quoteName()
+    {
+        return static::TABLE_QUOTE.$this->name.static::TABLE_QUOTE;
+    }
+
+    public function getRowCount()
+    {
+        return getValue('SELECT count(*) FROM '.$this->quoteName());
+    }
+    public function getRows($limit = 100)
+    {
+        return getObjects("SELECT * FROM ".$this->quoteName()." LIMIT ".(int)$limit);
+    }
+}
+class MySqlTableLister extends TableLister
+{
+    const TABLE_QUOTE = '`';
+
+    public static function getTables()
+    {
+        $objs = getObjects("SHOW TABLES");
+        $ret = array();
+        foreach ($objs as $obj) {
+            foreach ($obj as $v) {
+                $ret[] = new self($v);
+                break;
+            }
+        }
+        return $ret;
+    }
+    public function getColumns()
+    {
+        return getObjects("DESCRIBE ".$this->quoteName());
+    }
+}
+class PgSqlTableLister extends TableLister
+{
+    const TABLE_QUOTE = '"';
+
+    public static function getTables()
+    {
+        $objs = getObjects("select tablename from pg_tables where tableowner != 'postgres' order by tablename");
+        $ret = array();
+        foreach ($objs as $obj) {
+            $ret[] = new self($obj->tablename);
+        }
+        return $ret;
+    }
+    public function getColumns()
+    {
+        $sql = <<<SQL
 SELECT
     f.attname AS name,
     f.attnotnull AS notnull,
@@ -112,35 +133,40 @@ WHERE c.relkind = 'r'::char
     AND c.relname = ?  -- Replace with Schema name
     AND f.attnum > 0 ORDER BY f.attnum;
 SQL;
-    return getObjects($sql, $this->name);
-  }
+        return getObjects($sql, $this->name);
+    }
 }
-function prettyprint($data) {
-  if (count($data) == 0) {
-    echo '<p>Taulussa ei ole vielä dataa</p>';
-    return;
-  }
-  echo '<table class="table table-striped table-bordered">';
-  $tmp = array_values($data);
-  echo '<tr>',implode('', array_map(function($t) {return "<th>$t</th>";}, array_keys((array)array_shift($tmp)))),'</tr>';
-  foreach($data as $datum) {
-    echo '<tr>',implode('', array_map(function($t) {return "<td>$t</td>";}, (array)$datum)),'</tr>';
-  }
-  echo '</table>';
+function prettyprint($data)
+{
+    if (count($data) == 0) {
+        echo '<p>Taulussa ei ole vielä dataa</p>';
+        return;
+    }
+    echo '<table class="table table-striped table-bordered">';
+    $tmp = array_values($data);
+    echo '<tr>',implode('', array_map(function ($t) {
+        return "<th>$t</th>";
+    }, array_keys((array)array_shift($tmp)))),'</tr>';
+    foreach ($data as $datum) {
+        echo '<tr>',implode('', array_map(function ($t) {
+            return "<td>$t</td>";
+        }, (array)$datum)),'</tr>';
+    }
+    echo '</table>';
 }
 
 try {
-  $dbtype = $connection_config['db'];
-  switch($dbtype) {
-    case 'mysql':
-      $tables = MySqlTableLister::getTables();
-       break;
-    case 'psql':
-      $tables = PgSqlTableLister::getTables();
-      break;
-    default:
-      die("Unknown database");
-  }
+    $dbtype = $connection_config['db'];
+    switch ($dbtype) {
+        case 'mysql':
+            $tables = MySqlTableLister::getTables();
+            break;
+        case 'psql':
+            $tables = PgSqlTableLister::getTables();
+            break;
+        default:
+            die("Unknown database");
+    }
 ?>
 <!DOCTYPE html>
 <html>
@@ -160,19 +186,19 @@ try {
   </div>
 <div class="alert alert-success">Tietokantayhteyden muodostaminen onnistui!</div>
 <?php
-foreach($tables as $table): ?>
+foreach ($tables as $table) : ?>
   <h2><?= $table->getName(); ?></h2>
-  <?php prettyprint($table->getColumns()); ?>
+    <?php prettyprint($table->getColumns()); ?>
   <button type='button' class='btn-link expandable collapsed' data-toggle='collapse' data-target='#expandable_<?= $table->getName(); ?>'>
   Tietokantataulussa on yhteensä <?= $table->getRowCount(); ?> riviä:
   </button>
   <div id='expandable_<?= $table->getName(); ?>' class='collapse'>
-  <?php prettyprint($table->getRows()); ?>
+    <?php prettyprint($table->getRows()); ?>
   </div>
 <?php endforeach; ?>
 </div>
 </body>
 </html><?php } catch (Exception $e) {
-  echo '<pre>', $e->getMessage();
+    echo '<pre>', $e->getMessage();
 }
 ?>
