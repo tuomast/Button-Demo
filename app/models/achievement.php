e116dba2d0ccb852ff05af42695aad7905251fdc
Daniel Koch <daxda@cs.helsinki.fi>
2017-09-10 18:24:46 +0300

    All() funktio Achievementille

--- app/models/achievement.php
+++ app/models/achievement.php
@@ -2,10 +2,27 @@
 
 class achievement extends BaseModel{
 
-  public $id, $name, $description, $logo_url;
-  
+  public $id, $name, $description, $logo_url, $levels;
+
   public function __construct($attributes){
 		parent::__construct($attributes);
+	}
+
+  public static function all(){
+		$query = DB::connection()->prepare('SELECT * FROM Achievement');
+		$query->execute();
+		$rows = $query->fetchAll();
+		$achievements = array();
+		foreach($rows as $row){
+			$achievement[] = new Achievement(array(
+				'id' => $row['id'],
+				'name' => $row['name'],
+				'description' => $row['description'],
+				'logo_url' => $row['logo_url'],
+				'levels' => $row['levels']
+				));
+		}
+		return $achievements;
 	}
 
 }
